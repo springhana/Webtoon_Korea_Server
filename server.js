aeff8c451bc849804d236cc24002846831112652
springhana <xksxksanfro7@naver.com>
2023-09-24 22:37:08 +0900

    채팅 추가

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 
+// socket.io
+const http = require("http").createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(http, {
+  cors: {
+    origin: "*",
+  },
+});
+
 // env
 require("dotenv").config();
 
@@ -43,7 +52,7 @@ MongoClient.connect(url, { useUnifiedTopology: true }, function (에러, client)
 
   db = client.db("Webtoon"); // Webtoon 이라는 database(폴더)에 연결
 
-  app.listen(8080, function () {
+  http.listen(8080, function () {
     console.log("listening on 8080");
   });
 });
@@ -51,7 +60,95 @@ MongoClient.connect(url, { useUnifiedTopology: true }, function (에러, client)
 app.get("/", (req, res) => {
   res.send("성공");
 });
+// 이미지 업로드
+let multer = require("multer");
+const fs = require("fs");
+const { ObjectId } = require("mongodb");
+// 날짜
+const Today = (type) => {
+  const date = new Date();
+  const Y = date.getFullYear();
+  const M = date.getMonth() + 1;
+  const D = date.getDate();
 
+  const hours = date.getHours(); // 시간을 가져옵니다.
+  const minutes = date.getMinutes(); // 분을 가져옵니다.
+  const seconds = date.getSeconds(); // 초를 가져옵니다.
+  if (type === "day") {
+    return `${Y}-${M}-${D}`;
+  } else if (type === "time") {
+    return `${Y}-${M}-${D}:${hours}:${minutes}:${seconds}`;
+  }
+};
+
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    db.collection("count")
+      .findOne({ name: "게시물/댓글" })
+      .then((result) => {
+        console.log("url : " + req.url);
+        let _id = req.user ? req.user._id : null;
+        let date = Today("day");
+        let postNumber; // 게시판 기본키
+        let url;
+        let postComment; // 댓글 기본키
+
+        // PUT 요청에서만 postNumber를 추가
+        if (req.method === "PUT") {
+          postNumber = req.body.postNumber;
+          postComment = req.body.totalComment;
+        } else {
+          postNumber = result.totalPost + 1;
+          postComment = result.totalComment + 1;
+        }
+
+        console.log(req.url);
+        // url에 변경
+        if (req.url === "/write") {
+          url = "board";
+        } else if (req.url === "/edit") {
+          url = "board";
+        } else if (req.url === "/commentPost") {
+          postNumber = req.body.postNumber + "-" + postComment;
+          url = "comment";
+        } else if (req.url === "/editComment") {
+          postNumber = req.body.postNumber + "-" + postComment;
+          url = "comment";
+        }
+
+        let dir = `./public/image/${url}/${_id}/${date}/${postNumber}`;
+        if (req.url === "/register" || req.url === "/user/update") {
+          url = "user";
+          let id = req.body.id;
+          dir = `./public/image/${url}/${id}`;
+        }
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir, { recursive: true }); // recursive 옵션을 true로 설정하여 중간 디렉토리도 함께 생성
+        }
+
+        cb(null, dir);
+      });
+  },
+
+  filename: function (req, file, cb) {
+    // 이미지 이름 깨짐
+    file.originalname = Buffer.from(file.originalname, "latin1").toString(
+      "utf8"
+    );
+    cb(null, file.originalname.trim());
+  },
+  fileFilter: function (req, file, callback) {
+    var ext = path.extname(file.originalname);
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
+      return callback(new Error("PNG, JPG만 업로드하세요"));
+    }
+    callback(null, true);
+  },
+  limits: {
+    fileSize: 1024 * 1024,
+  },
+});
+var upload = multer({ storage: storage });
 // 로그인
 
 app.use(
@@ -119,7 +216,7 @@ passport.deserializeUser((id, done) => {
 });
 
 // 회원가입
-app.post("/register", (req, res) => {
+app.post("/register", upload.single("profile"), (req, res) => {
   db.collection("login").findOne({ id: req.body.id }, (error, result) => {
     if (result) {
       console.log("아이디있음");
@@ -130,10 +227,18 @@ app.post("/register", (req, res) => {
           pw: req.body.pw,
           name: req.body.name,
           email: req.body.email,
+          image: req.file
+            ? parseInt(req.body.deleteImg) === 0
+              ? req.file.filename
+              : "default.jpg"
+            : "default.jpg",
+          totalBoard: 0,
+          totalComment: 0,
         },
         (error, result) => {
           console.log("회원가입 성공");
           // 로그인 해주기
+          res.json({ register: true });
         }
       );
     }
@@ -279,90 +384,6 @@ app.get("/board", (req, res) => {
   // console.log(totalPosts);
 });
 
-// 이미지 업로드
-let multer = require("multer");
-const fs = require("fs");
-const { ObjectId } = require("mongodb");
-// 날짜
-const Today = (type) => {
-  const date = new Date();
-  const Y = date.getFullYear();
-  const M = date.getMonth() + 1;
-  const D = date.getDate();
-
-  const hours = date.getHours(); // 시간을 가져옵니다.
-  const minutes = date.getMinutes(); // 분을 가져옵니다.
-  const seconds = date.getSeconds(); // 초를 가져옵니다.
-  if (type === "day") {
-    return `${Y}-${M}-${D}`;
-  } else if (type === "time") {
-    return `${Y}-${M}-${D}:${hours}:${minutes}:${seconds}`;
-  }
-};
-let storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    db.collection("count")
-      .findOne({ name: "게시물/댓글" })
-      .then((result) => {
-        console.log("url : " + req.url);
-        let _id = req.user._id;
-        let date = Today("day");
-        let postNumber; // 게시판 기본키
-        let url;
-        let postComment; // 댓글 기본키
-
-        // PUT 요청에서만 postNumber를 추가
-        if (req.method === "PUT") {
-          postNumber = req.body.postNumber;
-          postComment = req.body.totalComment;
-        } else {
-          postNumber = result.totalPost + 1;
-          postComment = result.totalComment + 1;
-        }
-
-        console.log(req.url);
-        // url에 변경
-        if (req.url === "/write") {
-          url = "board";
-        } else if (req.url === "/edit") {
-          url = "board";
-        } else if (req.url === "/commentPost") {
-          postNumber = req.body.postNumber + "-" + postComment;
-          url = "comment";
-        } else if (req.url === "/editComment") {
-          postNumber = req.body.postNumber + "-" + postComment;
-          url = "comment";
-        }
-
-        let dir = `./public/image/${url}/${_id}/${date}/${postNumber}`;
-
-        if (!fs.existsSync(dir)) {
-          fs.mkdirSync(dir, { recursive: true }); // recursive 옵션을 true로 설정하여 중간 디렉토리도 함께 생성
-        }
-
-        cb(null, dir);
-      });
-  },
-  filename: function (req, file, cb) {
-    // 이미지 이름 깨짐
-    file.originalname = Buffer.from(file.originalname, "latin1").toString(
-      "utf8"
-    );
-    cb(null, file.originalname.trim());
-  },
-  fileFilter: function (req, file, callback) {
-    var ext = path.extname(file.originalname);
-    if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
-      return callback(new Error("PNG, JPG만 업로드하세요"));
-    }
-    callback(null, true);
-  },
-  limits: {
-    fileSize: 1024 * 1024,
-  },
-});
-var upload = multer({ storage: storage });
-
 // 글쓰기
 app.post("/write", upload.single("profile"), (req, res) => {
   db.collection("count")
@@ -381,6 +402,7 @@ app.post("/write", upload.single("profile"), (req, res) => {
             : "default.jpg"
           : "default.jpg",
         date: Today("time"),
+        likedIds: [],
       };
 
       db.collection("board")
@@ -396,8 +418,19 @@ app.post("/write", upload.single("profile"), (req, res) => {
             }
           );
           console.log("글쓰기 성공 : " + result);
-          res.json({ write: true });
         });
+      // 유저 게시판 수
+      db.collection("login").updateOne(
+        {
+          _id: ObjectId(req.user._id),
+        },
+        { $inc: { totalBoard: 1 } },
+        (error, result) => {
+          if (error) console.log(error);
+        }
+      );
+
+      res.json({ write: true });
     });
 });
 
@@ -462,6 +495,9 @@ app.get("/images", upload.single("profile"), (req, res) => {
       file = "board";
     } else if (req.query.file === "comment") {
       file = "comment";
+    } else if (req.query.file === "user") {
+      file = "user";
+      url = `${req.query.id}/${req.query.image}`;
     }
     console.log(file);
     res.sendFile(path.join(__dirname, `public/image/${file}`, url));
@@ -484,6 +520,17 @@ app.delete("/delete", (req, res) => {
       console.log(result);
     }
   });
+
+  // 유저 게시판 수
+  db.collection("login").updateOne(
+    {
+      _id: ObjectId(req.user._id),
+    },
+    { $inc: { totalBoard: -1 } },
+    (error, result) => {
+      if (error) console.log(error);
+    }
+  );
 });
 
 // 글 수정
@@ -524,6 +571,7 @@ app.post("/commentPost", upload.single("profile"), (req, res) => {
         boardId: req.body.boardId,
         userId: req.user._id,
         postNumber: parseInt(req.body.postNumber),
+        title: req.body.title,
         author: req.user.name,
         comment: req.body.comment,
         image: req.file
@@ -548,9 +596,19 @@ app.post("/commentPost", upload.single("profile"), (req, res) => {
             }
           );
           console.log("글쓰기 성공 : " + result);
-          res.json({ write: true });
         });
     });
+  // 유저 게시판 수
+  db.collection("login").updateOne(
+    {
+      _id: ObjectId(req.user._id),
+    },
+    { $inc: { totalComment: 1 } },
+    (error, result) => {
+      if (error) console.log(error);
+    }
+  );
+  res.json({ write: true });
 });
 // 게시판 댓글 가져오기
 app.get("/comment", (req, res) => {
@@ -580,6 +638,17 @@ app.delete("/commentDelete", (req, res) => {
       console.log(result);
     }
   });
+  // 유저 게시판 수
+  db.collection("login").updateOne(
+    {
+      _id: ObjectId(req.user._id),
+    },
+    { $inc: { totalComment: -1 } },
+    (error, result) => {
+      if (error) console.log(error);
+    }
+  );
+  res.json({ Delete: true });
 });
 
 // 댓글 수정을 위한 댓글 하나 가져오기
@@ -779,23 +848,45 @@ app.get("/user", (req, res) => {
       }
     });
 });
+
+// 상대 정보
+app.get("/profile", (req, res) => {
+  db.collection("login")
+    .findOne({ _id: ObjectId(req.query._id) })
+    .then((result) => {
+      if (result) {
+        console.log(result);
+        res.json(result);
+      }
+    });
+});
+
 // 자기정보 변경
-app.put("/user/update", (req, res) => {
-  let user = {
-    id: req.body.id,
-    pw: req.body.pw,
-    name: req.body.name,
-    email: req.body.email,
-  };
-  console.log(user);
-  db.collection("login").updateOne(
-    { _id: ObjectId(req.user._id) },
-    { $set: user },
-    (error, result) => {
-      console.log("로그인 수정 성공");
-      res.json({ edit: true });
-    }
-  );
+app.put("/user/update", upload.single("profile"), (req, res) => {
+  db.collection("login")
+    .findOne({ _id: ObjectId(req.user._id) })
+    .then((result) => {
+      let user = {
+        id: req.body.id,
+        pw: req.body.pw,
+        name: req.body.name,
+        email: req.body.email,
+        image: req.file
+          ? req.file.filename
+          : parseInt(req.body.deleteImg) === 0
+          ? result.image
+          : "default.jpg",
+      };
+      console.log(user);
+      db.collection("login").updateOne(
+        { _id: ObjectId(req.user._id) },
+        { $set: user },
+        (error, result) => {
+          console.log("로그인 수정 성공");
+          res.json({ edit: true });
+        }
+      );
+    });
 });
 
 // 댓글 개수 가져오기
@@ -809,9 +900,186 @@ app.get("/comment/length", (req, res) => {
 });
 
 // 내 글 가져오기
-app.use("/myBoard", (req, res) => {
+app.get("/myBoard", (req, res) => {
   db.collection("board")
     .find({ userId: ObjectId(req.user._id) })
+    .toArray()
+    .then((result) => {
+      res.json(result);
+    });
+});
+
+// 상대 글 가져오기
+app.get("/userBoard", (req, res) => {
+  db.collection("board")
+    .find({ userId: ObjectId(req.query._id) })
+    .toArray()
+    .then((result) => {
+      res.json(result);
+    });
+});
+
+// 채팅
+app.post("/chatroom", (req, res) => {
+  db.collection("login")
+    .findOne({ _id: ObjectId(req.body.chatuserId) })
+    .then((result) => {
+      const save = {
+        title: [result.name, req.user.name],
+        member: [ObjectId(req.body.chatuserId), req.user._id],
+        date: new Date(),
+      };
+
+      db.collection("chatroom")
+        .insertOne(save)
+        .then((result) => {
+          console.log("채팅방 생성");
+          res.json({ chatroom: true });
+        });
+    });
+});
+// 채팅방 가져오기
+app.get("/chat", Login, (req, res) => {
+  db.collection("chatroom")
+    .find({ member: req.user._id }) // array안에서 찾아도 가능
+    .toArray()
+    .then((result) => {
+      console.log("데이터 : " + result);
+      res.json(result);
+    });
+});
+
+// 채팅 메시지
+app.post("/message", Login, function (req, res) {
+  let save = {
+    chatroom: req.body.chatroom,
+    content: req.body.content,
+    userid: req.user.id,
+    userObjectId: req.user._id,
+    date: new Date(),
+  };
+  db.collection("message")
+    .insertOne(save)
+    .then(() => {
+      console.log("메시지 성공");
+      res.json({ message: true });
+    });
+});
+
+app.get("/messages", Login, function (req, res) {
+  db.collection("message")
+    .find({ chatroom: req.query.chatRoomId })
+    .toArray()
+    .then((result) => {
+      res.json(result);
+    });
+});
+
+// 채팅방 찾기
+app.get("/chatroomFind", (req, res) => {
+  db.collection("chatroom")
+    .findOne({ _id: ObjectId(req.query._id) })
+    .then((result) => {
+      res.json(result);
+    });
+});
+
+// 알림
+app.post("/notification", (req, res) => {
+  db.collection("notification")
+    .insertOne({
+      userId: req.body.userId,
+      date: req.body.date,
+    })
+    .then((result) => {
+      console.log("알림");
+      res.json({ notification: req.body.userId });
+    });
+});
+
+app.get("/notificationGet", (req, res) => {
+  db.collection("notification")
+    .findOne({ userId: req.query.userId })
+    .then((result) => {
+      res.json(result);
+      console.log(result);
+    });
+});
+
+// 웹 소켓
+
+io.on("connection", (socket) => {
+  console.log("클라이언트가 연결되었습니다.");
+
+  socket.on("message", (message, id, ids) => {
+    console.log("클라이언트로부터 메시지 받음:", message);
+    const date = new Date();
+    io.emit("message", message, date, id, ids); // 모든 클라이언트에게 메시지 전송
+  });
+});
+
+// 내 댓글 가져오기
+app.get("/myComment", (req, res) => {
+  db.collection("comment")
+    .find({ userId: ObjectId(req.query.userId) })
+    .toArray()
+    .then((result) => {
+      console.log(result);
+      res.json(result);
+    });
+});
+
+//  내 댓글 단 글 가져오기
+app.get("/myComment/board", (req, res) => {
+  db.collection("board")
+    .findOne({ _id: ObjectId(req.query._id) })
+    .then((result) => res.json(result));
+});
+
+// 만화 좋아요
+app.put("/board/like", Login, (req, res) => {
+  db.collection("board")
+    .findOne({ _id: ObjectId(req.body._id) })
+    .then((result) => {
+      let likedIds;
+      if (
+        result.likedIds.some(
+          (like) =>
+            ObjectId(like).toString() === ObjectId(req.user._id).toString()
+        )
+      ) {
+        console.log("중복");
+        // 좋아요 중복시 삭제
+        likedIds = result.likedIds.filter(
+          (like) =>
+            ObjectId(like).toString() !== ObjectId(req.user._id).toString()
+        );
+      } else {
+        // 좋아요 추가
+        console.log("추가");
+        likedIds = [...result.likedIds, req.user._id];
+      }
+
+      let comment = {
+        likedIds: likedIds,
+      };
+
+      db.collection("board").updateOne(
+        { _id: ObjectId(req.body._id) },
+        { $set: comment },
+        (error, result) => {
+          if (result) {
+            res.json({ like: true });
+          }
+        }
+      );
+    });
+});
+
+// 좋아요한 글 가져오기
+app.get("/board/liked", (req, res) => {
+  db.collection("board")
+    .find({ likedIds: ObjectId(req.query._id) })
     .toArray()
     .then((result) => {
       res.json(result);
